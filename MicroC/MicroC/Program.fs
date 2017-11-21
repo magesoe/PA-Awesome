759580d1f32a1a423e5ad95222fbb180c8e81d2c
Per Lange Laursen <perlangelaursen@gmail.com>
2017-11-21 22:09:53 +0100

    Parser and lexer test in progress

--- MicroC/MicroC/Program.fs
+++ MicroC/MicroC/Program.fs
@@ -1,5 +1,24 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open System.IO
+open System.Text
+open Microsoft.FSharp.Text.Lexing
+
+open Parser
+open Lexer
+
+let lexAndParseString str =
+    let lexbuf = LexBuffer<_>.FromString str
+    try 
+        Main tokenize lexbuf
+    with e ->
+        let pos = lexbuf.EndPos
+        printfn "Error near line %d, character %d\n" pos.Line pos.Column
+        failwith "parser termination";;
+    
+let lexAndParseFromFile filename =
+    if File.Exists(filename) then lexAndParseString(File.ReadAllText(filename))
+    else invalidArg "Program" "File not found";;
 
 [<EntryPoint>]
 let main argv = 
